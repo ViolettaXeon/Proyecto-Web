7554405012819e9d0fbd1e182be89904e37dfe05
Violetta <violettanoxx@gmail.com>
2026-05-29 09:32:52 -0500

    Update acercade.php

--- acercade.php
+++ acercade.php
@@ -29,7 +29,7 @@
 
             <!-- Imagen -->
             <img 
-                src="https://drive.google.com/file/d/1PUFwa-yDV1MXyvJEhPuSCcyiJvDoNazc/view?usp=drive_link"
+                src="img/violetta.jfif"
                 alt="Violetta"
                 style="
                     width: 180px;
